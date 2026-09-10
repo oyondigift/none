7583e674379a67ff0502092f71282662126d7496
Oyondi Gift <takeoffoyondi@gmail.com>
2026-09-10 13:56:26 +0300

    Delete login.py

--- login.py
+++
@@ -1,8 +0,0 @@
-password=input("enter your password\n")
-user=input("enter your user name\n")
-if password=="1234"and user=="takeoff":
-    print("authentication successful")
-else:
-    print("authentication failed")
-
-    
