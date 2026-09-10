88fc66314de2aabdf0281af0af3f5d9ae4a1bd9a
Oyondi Gift <takeoffoyondi@gmail.com>
2026-09-10 13:55:06 +0300

    Delete lambda2.py

--- lambda2.py
+++
@@ -1,7 +0,0 @@
-prod=lambda a,b,c:a*b*c
-#read the inputs
-x=int(input("enter the first number\n"))
-y=int(input("enter the second number\n"))
-z=int(input("enter the third number\n"))
-#call the function
-print(f"the product is \t {prod(x,y,z)}")
