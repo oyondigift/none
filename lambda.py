0c9de8abf7aaa0a8a31afcecd8d93bef1714b321
Oyondi Gift <takeoffoyondi@gmail.com>
2026-09-10 13:58:55 +0300

    Delete lambda.py

--- lambda.py
+++
@@ -1,9 +0,0 @@
-#Defining a function
-a=lambda l,w: l*w
-#calling the function and passing value
-print("The area is:",a(10,20))
-
-#Defining a function
-a_s=lambda PI,r: PI*r*r
-#calling the function and pasing the value
-print("the area is:",a_s(3.142,10))
